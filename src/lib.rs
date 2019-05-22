83b2ba9a4f5489799119e0e386f80ecd6eb98a4a
Rhys Newell <rhys.newell@uqconnect.edu.au>
2019-05-22 14:59:37 +1000

    Adding cargo files

---
+++ src/lib.rs
@@ -0,0 +1,89 @@
+pub mod bam_generator;
+pub mod filter;
+pub mod genomes_and_contigs;
+pub mod external_command_checker;
+pub mod bwa_index_maintenance;
+pub mod mapping_parameters;
+
+extern crate bio;
+#[macro_use]
+extern crate log;
+
+extern crate rust_htslib;
+extern crate env_logger;
+extern crate nix;
+extern crate tempdir;
+extern crate tempfile;
+extern crate rand;
+
+use std::path::Path;
+
+use genomes_and_contigs::GenomesAndContigs;
+
+
+
+pub const CONCATENATED_FASTA_FILE_SEPARATOR: &str = "~";
+pub fn read_genome_fasta_files(fasta_file_paths: &Vec<&str>)
+                               -> GenomesAndContigs {
+    let mut contig_to_genome = GenomesAndContigs::new();
+
+    for file in fasta_file_paths {
+        let path = Path::new(file);
+        let reader = bio::io::fasta::Reader::from_file(path)
+            .expect(&format!("Unable to read fasta file {}", file));
+
+        let genome_name = String::from(
+            path.file_stem()
+                .expect("Problem while determining file stem")
+                .to_str()
+                .expect("File name string conversion problem"));
+        if contig_to_genome.genome_index(&genome_name).is_some() {
+            panic!("The genome name {} was derived from >1 file", genome_name);
+        }
+        let genome_index = contig_to_genome.establish_genome(genome_name);
+        for record in reader.records() {
+            let contig = String::from(
+                record
+                    .expect(&format!("Failed to parse contig name in fasta file {:?}", path))
+                    .id());
+            contig_to_genome.insert(contig, genome_index);
+        }
+    }
+    return contig_to_genome;
+}
+
+#[derive(PartialEq, Debug)]
+pub struct ReadsMapped {
+    num_mapped_reads: u64,
+    num_reads: u64
+}
+
+#[derive(Clone, Debug)]
+pub struct FlagFilter {
+    pub include_improper_pairs: bool,
+    pub include_supplementary: bool,
+    pub include_secondary: bool,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_contig_to_genome(){
+        let mut contig_to_genome = GenomesAndContigs::new();
+        let genome = String::from("genome0");
+        let index = contig_to_genome.establish_genome(genome);
+        contig_to_genome.insert(String::from("contig1"), index);
+        assert_eq!(
+            String::from("genome0"),
+            *(contig_to_genome.genome_of_contig(&String::from("contig1")).unwrap()));
+    }
+
+    #[test]
+    fn test_read_genome_fasta_files_one_genome(){
+        let contig_to_genome = read_genome_fasta_files(&vec!["tests/data/genome1.fna"]);
+        assert_eq!(String::from("genome1"), *contig_to_genome.genome_of_contig(&String::from("seq1")).unwrap());
+        assert_eq!(String::from("genome1"), *contig_to_genome.genome_of_contig(&String::from("seq2")).unwrap());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate strainm;
+use strainm::bam_generator::*;
+use strainm::bam_generator::*;
+use strainm::filter;
+use strainm::external_command_checker;
+use strainm::mapping_parameters::*;
+use strainm::FlagFilter;
+use strainm::CONCATENATED_FASTA_FILE_SEPARATOR;
+use strainm::genomes_and_contigs::GenomesAndContigs;
+
+
+extern crate rust_htslib;
+use rust_htslib::bam;
+use rust_htslib::bam::Read;
+
+use std::env;
+use std::str;
+use std::process;
+
+extern crate clap;
+use clap::*;
+
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+use log::LevelFilter;
+use env_logger::Builder;
+
+extern crate tempfile;
+use tempfile::NamedTempFile;
+#[macro_use]
+extern crate lazy_static;
+
+
 fn main() {
-    println!("Hello, world!");
+
 }
