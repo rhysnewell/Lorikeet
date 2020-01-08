f9f900ce964e94352b09e6223a859672a04471a0
rhysnewell <rhys.newell@uqconnect.edu.au>
2020-01-08 16:51:23 +1000

    parralelizing distance matrix and NMF calc

--- src/nmf.py
+++ src/nmf.py
@@ -4,44 +4,56 @@ from scipy.spatial.distance import squareform
 import sys, os
 import ctypes
 import ast
+import random
 
-def perform_nmf(array, k=10, miter=10, rrange=range(2,20)):
+#random.seed(a=345210)
+
+def perform_nmf(array, k=10, miter=10, estimateRanks='True'):
     # array = [[] for i in filenames]
-    bd = nimfa.Nmf(array, seed='nndsvd', rank=k, max_iter=miter, update='euclidean',
-                objective='fro')
+    bd = nimfa.Nsnmf(array, seed='nndsvd', rank=k, max_iter=miter, update='euclidean',
+                    objective='fro')
     bd_fit = bd()
-    print('Rss: %5.4f' % bd_fit.fit.rss())
-    print('Evar: %5.4f' % bd_fit.fit.evar())
-    print('K-L divergence: %5.4f' % bd_fit.distance(metric='kl'))
-    print('Sparseness, W: %5.4f, H: %5.4f' % bd_fit.fit.sparseness())
+    if estimateRanks == 'True':
+        print(bd_fit.fit.rss())
+    else:
+        print('Rss: %5.4f' % bd_fit.fit.rss())
+        print('Evar: %5.4f' % bd_fit.fit.evar())
+        print('K-L divergence: %5.4f' % bd_fit.distance(metric='kl'))
+        print('Sparseness, W: %5.4f, H: %5.4f' % bd_fit.fit.sparseness())
+        predictions = bd_fit.fit.predict(prob=True)
+        bins = np.array(predictions[0])[0]
+        print(predictions)
+    #print('Evar: %5.4f' % bd_fit.fit.evar())
+    #print('K-L divergence: %5.4f' % bd_fit.distance(metric='kl'))
+    #print('Sparseness, W: %5.4f, H: %5.4f' % bd_fit.fit.sparseness())
 
     # lsnmf = nimfa.SepNmf(array, seed='random_vcol', rank=k, n_run=10)
-    estimated_ranks = bd.estimate_rank(rank_range=rrange, n_run=miter//2)
+    #estimated_ranks = bd.estimate_rank(rank_range=rrange, n_run=miter//2)
 
-    best_rank = 0
-    best_rss = None
-    # Choose the best rank by finding the first inflection point in the RSS values
-    # Quick method is to find first point when RSS is at lowest value
-    for rank, values in estimated_ranks.items():
-        print('Rank: %d' % rank)
-        print('Rss: %5.4f' % values['rss'])
-        print('Evar: %5.4f' % values['evar'])
-        print('K-L: %5.4f' % values['kl'])
-        if best_rss is None or values['rss'] < best_rss:
-            best_rank = rank
-            best_rss = values['rss']
-        elif values['rss'] >= best_rss:
-            break
+    #best_rank = 0
+    #best_rss = None
+    ## Choose the best rank by finding the first inflection point in the RSS values
+    ## Quick method is to find first point when RSS is at lowest value
+    #for rank, values in estimated_ranks.items():
+    #    print('Rank: %d' % rank)
+    #    print('Rss: %5.4f' % values['rss'])
+    #    print('Evar: %5.4f' % values['evar'])
+    #    print('K-L: %5.4f' % values['kl'])
+    #    if best_rss is None or values['rss'] < best_rss:
+    #        best_rank = rank
+    #        best_rss = values['rss']
+    #    elif values['rss'] >= best_rss:
+    #        break
 
-    print("%d strains estimated" % best_rank)
-    bd = nimfa.Nmf(array, seed='nndsvd', rank=best_rank, max_iter=miter, update='euclidean',
-                                         objective='fro')
+    #print("%d strains estimated" % best_rank)
+    #bd = nimfa.Nmf(array, seed='nndsvd', rank=best_rank, max_iter=miter, update='euclidean',
+    #                                     objective='fro')
 
-    bd_fit = bd()
-    print('Rss: %5.4f' % bd_fit.fit.rss())
-    print('Evar: %5.4f' % bd_fit.fit.evar())
-    print('K-L divergence: %5.4f' % bd_fit.distance(metric='kl'))
-    print('Sparseness, W: %5.4f, H: %5.4f' % bd_fit.fit.sparseness())
+    #bd_fit = bd()
+    #print('Rss: %5.4f' % bd_fit.fit.rss())
+    #print('Evar: %5.4f' % bd_fit.fit.evar())
+    #print('K-L divergence: %5.4f' % bd_fit.distance(metric='kl'))
+    #print('Sparseness, W: %5.4f, H: %5.4f' % bd_fit.fit.sparseness())
 
 
 
@@ -54,11 +66,11 @@ def perform_nmf(array, k=10, miter=10, rrange=range(2,20)):
 if __name__=="__main__":
     try:
         pairwise_distances = sys.argv[4]
-        minrank = int(sys.argv[1])
-        maxrank = int(sys.argv[2])
+        minRank = int(sys.argv[1])
+        estimateRanks = sys.argv[2]
         miter = int(sys.argv[3])
     except IndexError:
-        print("Usage <Ranks> <Max Iterations> <Rank Range> <Input Pairwise Distance Vector>")
+        print("Usage <Ranks> <Estimate Ranks> <Max Iterations> <Input Pairwise Distance Vector>")
         sys.exit()
 
     condensed_vec = []
@@ -69,5 +81,5 @@ if __name__=="__main__":
     condensed_vec = np.array(condensed_vec)
     square_vec = squareform(condensed_vec)
 
-    perform_nmf(square_vec, minrank, miter, range(minrank, maxrank+1))
+    perform_nmf(square_vec, minRank, miter, estimateRanks)
 
