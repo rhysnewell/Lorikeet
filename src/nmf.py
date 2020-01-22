86819062fb8f3681137ccd7721f5cbec4230ef23
rhysnewell <rhys.newell@uqconnect.edu.au>
2020-01-22 14:51:43 +1000

    potentially working multi sample version, thread limit on numpy

--- src/nmf.py
+++ src/nmf.py
@@ -3,6 +3,7 @@
 #warnings.filterwarnings("ignore")
 import nimfa
 #warnings.filterwarnings("always")
+from threadpoolctl import threadpool_limits
 import numpy as np
 from scipy.spatial.distance import squareform
 import sys
@@ -11,23 +12,25 @@ import sys
 
 def perform_nmf(array, constraints, k=10, miter=10, estimateRanks='True', path='/tmp/lorikeet-pred'):
     # array = [[] for i in filenames]
-    # bd = nimfa.Nsnmf(array, seed='nndsvd', rank=k, max_iter=miter, update='euclidean',
+    # mf = nimfa.Nsnmf(array, seed='nndsvd', rank=k, max_iter=miter, update='euclidean',
     #                 objective='conn')
     if constraints is None:
-        bd = nimfa.Bd(array, seed='nndsvd', rank=k, max_iter=miter)
-        bd_fit = bd()
+        mf = nimfa.Nmf(array, seed='nndsvd', rank=k, max_iter=miter, update='euclidean',
+                        objective='conn')
+        # mf = nimfa.Psmf(array, seed=None, rank=k, max_iter=miter)
+        mf_fit = mf()
         if estimateRanks == 'True':
-            print(bd_fit.fit.rss())
+            print(mf_fit.fit.rss())
 
         else:
             print('Rank: %d' % k)
-            print('Rss: %5.4f' % bd_fit.fit.rss())
-            print('Evar: %5.4f' % bd_fit.fit.evar())
-            print('K-L divergence: %5.4f' % bd_fit.distance(metric='kl'))
-            print('Sparseness, W: %5.4f, H: %5.4f' % bd_fit.fit.sparseness())
-            print('Connectivity', bd_fit.fit.connectivity())
+            print('Rss: %5.4f' % mf_fit.fit.rss())
+            print('Evar: %5.4f' % mf_fit.fit.evar())
+            print('K-L divergence: %5.4f' % mf_fit.distance(metric='kl'))
+            print('Sparseness, W: %5.4f, H: %5.4f' % mf_fit.fit.sparseness())
+            print('Connectivity', mf_fit.fit.connectivity())
 
-            predictions = bd_fit.fit.predict(prob=True)
+            predictions = mf_fit.fit.predict(prob=True)
             bins = np.array(predictions[0])[0]
             # bins = np.array(predictions[0])[0].reshape((len(bins), 1))
             new_pred = np.column_stack((bins, np.array(predictions[1])))
@@ -35,10 +38,10 @@ def perform_nmf(array, constraints, k=10, miter=10, estimateRanks='True', path='
 
             np.save(path, new_pred.astype('float32'), False)
     else:
-        bd = nimfa.Pmfcc(array, seed='nndsvd', rank=k, max_iter=miter, theta=constraints)
-        bd_fit = bd()
+        mf = nimfa.Pmfcc(array, seed='nndsvd', rank=k, max_iter=miter, theta=constraints)
+        mf_fit = mf()
         if estimateRanks == 'True':
-            print(bd_fit.distance())
+            print(mf_fit.distance())
 
 
 
@@ -49,6 +52,7 @@ if __name__=="__main__":
         estimateRanks = sys.argv[2]
         miter = int(sys.argv[3])
         sample_count = int(sys.argv[6])
+        threads = int(sys.argv[7])
         try:
             # constraints = np.load(sys.argv[5])
             constraints = None
@@ -56,14 +60,15 @@ if __name__=="__main__":
             constraints = None
 
     except IndexError:
-            print("Usage <Ranks> <Estimate Ranks> <Max Iterations> <Input Pairwise Distance Vector> <Sample Count>")
+            print("Usage <Ranks> <Estimate Ranks> <Max Iterations> <Input Pairwise Distance Vector> <BLANK> <Sample Count> <Threads>")
             sys.exit()
 
-    if sample_count > 0:
-        pairwise_distances = squareform(pairwise_distances)
-        if constraints is not None:
-            constraints = squareform(constraints)
+    with threadpool_limits(limits=threads, user_api='blas'):
+        if sample_count > 0:
+            pairwise_distances = squareform(pairwise_distances)
+            if constraints is not None:
+                constraints = squareform(constraints)
 
-    pairwise_distances = pairwise_distances / np.linalg.norm(pairwise_distances)
-    perform_nmf(pairwise_distances, constraints, minRank, miter, estimateRanks, sys.argv[4])
+        pairwise_distances = pairwise_distances / np.linalg.norm(pairwise_distances)
+        perform_nmf(pairwise_distances, constraints, minRank, miter, estimateRanks, sys.argv[4])
 
