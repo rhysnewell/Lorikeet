8175b94d680b0a66d8fc528ad0020a3bd60f6775
Rhys Newell <s4331221@page.ace.uq.edu.au>
2020-06-08 17:32:20 +1000

    adding build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+PREFIX=$CONDA_PREFIX
+echo $PREFIX
+
+# Build statically linked binary with Rust
+C_INCLUDE_PATH=$PREFIX/include \
+LIBRARY_PATH=$PREFIX/lib \
+LIBCLANG_PATH=$PREFIX/lib/libclang.so \
+cargo build --release
+
+# Install the binaries
+C_INCLUDE_PATH=$PREFIX/include \
+LIBRARY_PATH=$PREFIX/lib \
+LIBCLANG_PATH=$PREFIX/lib/libclang.so \
+cargo install --root $PREFIX
+
+# Backup install
+# cp target/release/lorikeet $CONDA_PREFIX/bin/ && cp target/release/remove_minimap2_duplicated_headers $CONDA_PREFIX/bin/
