ccfb6b8843e38a05ccecd2edb904d8af97fb4738
rhysnewell <rhys.newell@uqconnect.edu.au>
2020-07-09 09:57:38 +1000

    rho-M now working again

--- src/dbscan/fuzzy.rs
+++ src/dbscan/fuzzy.rs
@@ -107,10 +107,10 @@ impl MetricSpace for Var<> {
 //                let jaccard = intersection.len() as f64 /
 //                    (set1.len() + set2.len() + 1) as f64;
 
-//                debug!("Phi {}, Phi-D {}, concordance {}, Rho {}, Rho-M {}, row_var {} col_var {} covar {}",
-//                       phi, phi_dist, concordance, rho, 1.-rho, row_var, col_var, covar);
+                trace!("Phi {}, Phi-D {}, concordance {}, Rho {}, Rho-M {}, row_var {} col_var {} covar {}",
+                       phi, phi_dist, concordance, rho, 1.-rho, row_var, col_var, covar);
 
-                return concordance
+                return 1.-rho
             }
         } else {
             return if self.pos == other.pos && self.tid == other.tid {
