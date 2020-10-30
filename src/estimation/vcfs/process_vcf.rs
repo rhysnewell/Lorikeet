fc3f6706efa9c90a4789aedaabacab7c544ca558
Rhys Newell <rhys.newell@hdr.qut.edu.au>
2020-10-30 16:03:05 +1000

    remvoing longread bool check

--- src/estimation/vcfs/process_vcf.rs
+++ src/estimation/vcfs/process_vcf.rs
@@ -252,7 +252,7 @@ pub fn process_vcf<'b, R: IndexedNamedBamReader + Send, G: NamedBamReaderGenerat
     let mut variant_matrix_sync = Arc::new(Mutex::new(variant_matrix.clone()));
     let freebayes_threads = std::cmp::max(split_threads, 1);
 
-    if longread || !longread {
+    if longread {
         //placeholder check
         // Get VCF file from BAM using freebayes of SVIM
         let mut vcf_reader = get_vcf(
